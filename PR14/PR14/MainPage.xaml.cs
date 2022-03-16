c0977660ec4fbd1758947a2ac08bf66d7133b29d
79510 <79510@DESKTOP-F5O79UK>
2022-03-16 23:11:03 +0300

    Добавьте файлы проекта.

---
+++ PR14/PR14/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PR14
+{
+    public partial class MainPage : ContentPage
+    {
+        Button timerButton;
+        bool alive = true;
+        public MainPage()
+        {
+            timerButton = new Button
+            {
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalOptions = LayoutOptions.Center,
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Button))
+            };
+            timerButton.Clicked += TimerButton_Clicked;
+
+            Content = timerButton;
+
+            DisplayTime();
+        }
+        private async void DisplayTime()
+        {
+            while (alive)
+            {
+                timerButton.Text = DateTime.Now.ToString("T");
+                await Task.Delay(1000);
+            }
+        }
+        private void TimerButton_Clicked(object sender, EventArgs e)
+        {
+            if (alive == true)
+            {
+                alive = false;
+            }
+            else
+            {
+                alive = true;
+                DisplayTime();
+            }
+        }
+    }
+}
